3c3248761342a2cc7aaa627119955aeb73924658
Кирилл Николаенко <panuka@MacBook-Pro-Kirill.local>
2015-12-03 01:23:46 +0300

    fix sample

--- config.sample.php
+++ config.sample.php
@@ -1,4 +1,17 @@
 <?php
 
-global $token;
 $token = 'YOUR_TELEGRAM_ACCESS_TOKEN';
+
+$answers = array(
+    array(
+        'REGEXP1_ANSWER1', 'ANSWER2'
+    ),
+    array(
+        'WORD2_ANSWER_1', 'WORD2_ANSWER_2'
+    ),
+);
+
+$regexps = array(
+    'WORD_WHICH_FIND_1',
+    'WORD_WHICH_FIND_2',
+);
